170f40ce47181a4a09d3a4636914700f43fb2164
tejitak <tejitak@gmail.com>
2016-11-12 12:57:18 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
+var apiai = require('apiai');
 /* GET home page. */
 
 //verification code
@@ -16,47 +17,49 @@ router.get('/webhook', function(req, res) {
    res.send('Error, wrong validation token');
  }
 });
+const ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN;
+const API_AI_ACCESS_TOKEN = process.env.API_AI_ACCESS_TOKEN
 
-//recievemessage
-router.post('/webhook/', function (req, res) {
- const events = req.body.entry[0].messaging;
- for (i = 0; i < events.length; i++) {
-   const event = req.body.entry[0].messaging[i];
-   const sender = event.sender.id;
-   if (event.message && event.message.text) {
-     const text = event.message.text;
-     console.log(text)
-     sendTextMessage(sender, "Text received!! echo: " + text);
-   }
- }
-
- res.sendStatus(200);
-
+router.post('/webhook', function (req, res) {
+  const events = req.body.entry[0].messaging;
+  var app = apiai(API_AI_ACCESS_TOKEN);
+  for (i = 0; i < events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      var airequest = app.textRequest(text);
+      airequest.on('response', function(response) {
+        console.log(response);
+        sendTextMessage(sender, response.result.fulfillment.speech);
+      });
+      airequest.on('error', function(error) {
+        console.log(error);
+      });
+      airequest.end();
+    }
+  }
+  res.sendStatus(200);
 });
-
-//POST message 
-var request = require('request')
-
-const ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN
-
-
 function sendTextMessage(sender, text) {
- request({
-   url: 'https://graph.facebook.com/v2.6/me/messages',
-   qs: {access_token: ACCESS_TOKEN},
-   method: 'POST',
-   json: {
-     recipient: {id: sender},
-     message: {text: text}
-   }
-
- }, function(error, response, body) {
-   if (error) {
-     console.log('Error sending message: ', error);
-   } else if (response.body.error) {
-     console.log('Error: ', response.body.error);
-   }
- });
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender
+      },
+      message: {
+        text: text
+      }
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 }
-
 module.exports = router;
